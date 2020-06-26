f9a29bfbc6d850e6b72639cc323ac47e48a8b4c9
jaab30 <jaab30@yahoo.com>
2020-06-26 05:48:23 -0400

    Google Auth Completed - redux implementation

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import StreamList from '../pages/StreamList';
+import StreamCreate from '../pages/StreamCreate';
+import StreamDelete from '../pages/StreamDelete';
+import StreamEdit from '../pages/StreamEdit';
+import StreamShow from '../pages/StreamShow';
+import Header from "./Header";
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <Router>
+                <Header />
+                <Route path="/" exact component={StreamList} />
+                <Route path="/edit" exact component={StreamEdit} />
+                <Route path="/create" exact component={StreamCreate} />
+                <Route path="/delete" exact component={StreamDelete} />
+                <Route path="/show" exact component={StreamShow} />
+            </Router>
+        </div>
+    )
+}
+
+export default App
+
+
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers"
+import App from "./components/App";
+
+
+// const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (a) => a);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector("#root")
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
